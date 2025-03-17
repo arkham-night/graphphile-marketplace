dee2790ab79f5632eab7131850302e6b67a41e39
arkham-night <65436266+arkham-night@users.noreply.github.com>
2025-03-17 06:39:47 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect } from 'react';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
@@ -14,7 +13,11 @@ const Index = () => {
     <div className="min-h-screen flex flex-col">
       <Navbar />
       <main className="flex-grow">
-        <Hero />
+        <Hero 
+          title="GraphPhile" 
+          subtitle="Express your creativity with our designer graphic tees"
+          bgImage="https://images.unsplash.com/photo-1529374255404-311a2a4f1fd9?ixlib=rb-4.0.3&auto=format&fit=crop&w=1920&q=80" 
+        />
         <div className="container mx-auto px-4 py-8">
           <FeaturedCollection />
         </div>
